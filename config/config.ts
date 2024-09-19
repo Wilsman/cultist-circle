ed5331c72a26b3ddcf551b27d2bd08613cbd4378
Wilsman <myleswilson77@hotmail.com>
2024-09-19 22:40:23 +0100

    feat: expand IGNORED_ITEMS in config to include additional items

--- config/config.ts
+++ config/config.ts
@@ -2,4 +2,9 @@
 
 export const FILTER_TAGS: string[] = ["Barter", "Provisions", "Containers"];
 
-export const IGNORED_ITEMS: string[] = ["Metal fuel tank (0/100)"];
+export const IGNORED_ITEMS: string[] = [
+  "Metal fuel tank (0/100)",
+  "Expeditionary fuel tank (0/60)",
+  "Aquamari water bottle with filter",
+  "FP-100 filter absorber",
+];
