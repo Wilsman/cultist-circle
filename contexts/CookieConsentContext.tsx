d45841bb2aa7c456f920974467d5dc3383ad91b5
Wilsman <myleswilson77@hotmail.com>
2024-10-17 22:59:40 +0100

    Refactor CookieConsentContext to include accept and decline functions, and set initial cookie acceptance to true TODO: fully impliment this!

--- contexts/CookieConsentContext.tsx
+++ contexts/CookieConsentContext.tsx
@@ -1,46 +1,54 @@
-"use client"
+"use client";
 
-import React, { createContext, useContext, useState, useEffect } from 'react'
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 type CookieConsentContextType = {
-  cookiesAccepted: boolean | null
-  acceptCookies: () => void
-  declineCookies: () => void
-}
+  cookiesAccepted: boolean | null;
+  acceptCookies: () => void;
+  declineCookies: () => void;
+};
 
-const CookieConsentContext = createContext<CookieConsentContextType | undefined>(undefined)
+const CookieConsentContext = createContext<
+  CookieConsentContextType | undefined
+>(undefined);
 
 export const useCookieConsent = () => {
-  const context = useContext(CookieConsentContext)
+  const context = useContext(CookieConsentContext);
   if (context === undefined) {
-    throw new Error('useCookieConsent must be used within a CookieConsentProvider')
+    throw new Error(
+      "useCookieConsent must be used within a CookieConsentProvider"
+    );
   }
-  return context
-}
+  return context;
+};
 
-export const CookieConsentProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [cookiesAccepted, setCookiesAccepted] = useState<boolean | null>(null)
+export const CookieConsentProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [cookiesAccepted, setCookiesAccepted] = useState<boolean | null>(true);
 
   useEffect(() => {
-    const consent = localStorage.getItem('cookieConsent')
+    const consent = localStorage.getItem("cookieConsent");
     if (consent !== null) {
-      setCookiesAccepted(consent === 'true')
+      setCookiesAccepted(consent === "true");
     }
-  }, [])
+  }, []);
 
   const acceptCookies = () => {
-    setCookiesAccepted(true)
-    localStorage.setItem('cookieConsent', 'true')
-  }
+    setCookiesAccepted(true);
+    localStorage.setItem("cookieConsent", "true");
+  };
 
   const declineCookies = () => {
-    setCookiesAccepted(false)
-    localStorage.setItem('cookieConsent', 'false')
-  }
+    setCookiesAccepted(false);
+    localStorage.setItem("cookieConsent", "false");
+  };
 
   return (
-    <CookieConsentContext.Provider value={{ cookiesAccepted, acceptCookies, declineCookies }}>
+    <CookieConsentContext.Provider
+      value={{ cookiesAccepted, acceptCookies, declineCookies }}
+    >
       {children}
     </CookieConsentContext.Provider>
-  )
-}
+  );
+};
