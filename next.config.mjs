0f4c1d1413d69dae8295f38b1fbc35399bd88c00
Wilsman <myleswilson77@hotmail.com>
2026-09-06 22:23:19 +0100

    perf(images): remove remote image patterns to block optimizer usage
    [v2.1.2]

--- next.config.mjs
+++ next.config.mjs
@@ -42,28 +42,6 @@ const nextConfig = {
       },
     ];
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.buymeacoffee.com",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.tarkov.dev",
-      },
-      {
-        protocol: "https",
-        hostname: "pub-226fae05b0214cbeb8e3cb97c8fb6293.r2.dev",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.cultistcircle.com",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
